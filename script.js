a88c28c9beb768329c2d10e603f8d2fcbdc99089
Radhika Irkal <radhikamirkal@gmail.com>
2026-02-23 20:03:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,28 +3,21 @@ function Person(name, age) {
     this.age = age;
 }
 
-// greet method
 Person.prototype.greet = function() {
-    console.log(`Hello, my name is ${this.name}, I am ${this.age} years old.`);
+    console.log(`Hello, my name is ${this.name} and I am ${this.age} years old.`);
 };
 
 function Employee(name, age, jobTitle) {
-    // inherit properties
     Person.call(this, name, age);
     this.jobTitle = jobTitle;
 }
 
-// inherit prototype methods
 Employee.prototype = Object.create(Person.prototype);
-
-// fix constructor reference
 Employee.prototype.constructor = Employee;
 
-// jobGreet method
 Employee.prototype.jobGreet = function() {
     console.log(`Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}.`);
 };
 
-// Do not change code below
 window.Person = Person;
 window.Employee = Employee;
